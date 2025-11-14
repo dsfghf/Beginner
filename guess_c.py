c0bd9d4f559f91147fe39d3f6f2e6b1a3d892e63
Basma <117016549+dsfghf@users.noreply.github.com>
2025-11-14 14:54:01 +0300

    Update guess_c.py

--- guess_c.py
+++ guess_c.py
@@ -1,30 +1,31 @@
 import random
-name=input("Whats your name? ")
-print(f"Welcome {name} in Guessing game !!! ")
-words=['hello','Hello','Hi','banan','apple','mango']
+name=input("Whats your name ?")
+print(f"Welcome {name} in guessing game!!!")
+words=['nasma','basma','hamsa','banan','mango']
 word=random.choice(words)
 print("Guess the characters ")
-guesses=''
-turns=14
+guesses=' '
+turns=15
 while turns>0:
-       
-        failed=0
-        for char in word:
-           if char in guesses:
-             print(char,end=" ")
-           else:
-             print("_")
-             failed+=1
-        if failed==0:
-            print(" \nYOU win ")
-            print(f"The word is :: {word}")
-            break
-        print()
-        guess=input("guess a character:: ")
-        guesses+=guess
-        if guess not in word:
-            turns-=1
-            print("Wrong")
-            print(f"You have {turns},more guesses ")
-            if turns==0:
-                print("You loose")
+    failed=0
+    for char in word:
+        if char in guesses:
+            print(char,end=' ')
+        else:
+            print("_")
+            failed+=1
+    if failed ==0:
+        print("You Win")
+        print("The word is : ",word)
+        break
+    
+    print()
+    guess=input("guess a character:: ")
+    guesses+=guess
+    if guess not in word:
+        turns-=1
+        print("Wrong")
+        print(f"You have {turns},more guesses ")
+        if turns==0:
+            print("You loose! ")
+        
