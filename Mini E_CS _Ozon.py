cdb5bc43a4001e90495c4a2284db31781a07597f
Basma <117016549+dsfghf@users.noreply.github.com>
2025-11-21 17:59:12 +0300

    Registration
    Verify login to the site

--- Mini E_CS _Ozon.py
+++ Mini E_CS _Ozon.py
@@ -4,7 +4,8 @@ class ozon:
         print("Welcome to OZON!\n")
 
     def __init__(self):
-        self.dt = {}
+        self.users=[]
+        self.current_user=None
 
     def n_data(self):
       
@@ -13,9 +14,7 @@ class ozon:
             name = input("Please enter your name: ")
               
             if name.isalpha():
-                self.dt["name"] = name
-                print(f"Welcome {name}\n")
-                break
+               return name
             else:
                 print("your name must contain letteres only , Try again Please !\n")
     def e_data(self):
@@ -28,8 +27,7 @@ class ozon:
                   
                   continue
               else:
-                  self.dt["age"]=age
-                  return True
+                  return age
                  
            except ValueError:
                print("Please Enter a valid number for age! \n")
@@ -43,32 +41,69 @@ class ozon:
                 print("Invalid email format! Try again.\n")
                 continue
             else:
-                self.dt["email"]=email
-                print(f" Hello {self.dt['name']} your acount has been created! \n")
-                break
+                return email
+    
+               
     def register(self):
-        if "name" in self.dt and "age" in self.dt:
-            print("Your Data already exists!\nTo complete the login Enter your email\n")
+       print("\n ---- Register New Acount -----")
             
+       name=self.n_data()
+       age=self.e_data()
+       email=self.em_data()
+       for user in self.users:
+           if user["email"]==email:
+              print("Email already exists! Please login instead .\n")
+              return False
+         
+               
+       user={"name":name,"age":age,"email":email}
+       self.users.append(user)
+       self.current_user=user
+       print(f"Hello {name} ,your account has been created! \n")
+       return True
        
+    def login(self):
+        print("-----Login----- ")
+        email=input("Enter your email: ").strip()
+        for user in self.users:
+            if user["email"]==email:
+                self.current_user=user
+                print(f" Welcome back {user['name']} !\n")
+                return True
+           
+        print("Email not found .Please register first.\n")
+        return False
 
     def check_out(self):
         required=["name","age","email"]
     #    if all(key in self.dt for key in["name","age","email"]):
-        for i in required:
-            if i not in self.dt:
+        if self.current_user is None:
+            print("No user logged in !\n")
+            return False
+        for k in required:
+            if k not in self.current_user:
                 
-                print("Some data is missing. Please register first.\n")
+                print("Some data is missing. Please complete registration .\n")
                 return False
-            else:
-                print("All data found! You are now logged in .\n")
-            return True
+           
+        print("All data found! You are now logged in .\n")
+        return True
 
 
-oz = ozon()
+oz=ozon()
 oz.hi()
-oz.n_data()
-oz.e_data()
-oz.em_data()
-oz.register()
+
+while True :
+    choice=int(input ("Do you want to (1)Register or (2) Login ? Enter 1 or 2: "))
+    if choice==1:
+        if oz.register():
+           break
+    elif choice==2:
+        if oz.login():
+           break
+    else:
+        print("Invalid choice! Please enter 1 or 2 .\n")   
+        
+
+
 oz.check_out()
