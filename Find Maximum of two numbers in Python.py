deda596f48338f036b960a6336e429f3bdee5429
Basma <117016549+dsfghf@users.noreply.github.com>
2025-12-06 17:44:33 +0300

    Update Find Maximum of two numbers in Python.py

--- Find Maximum of two numbers in Python.py
+++ Find Maximum of two numbers in Python.py
@@ -6,3 +6,18 @@ print(max(a,b))
 w=2
 c=8
 print(w if w>c else c)
+
+#Using if-Else statement
+r=3
+t=8
+if r>t:
+    print(r)
+else:
+    print(t)
+
+#Using sort()
+o=4
+p=43
+num=[o,p]
+num.sort()
+print(num[-1])
